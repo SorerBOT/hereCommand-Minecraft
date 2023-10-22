bacdae26701e486b242ae98807651329b703fa44
SorerBOT <alonfiller@gmail.com>
2023-10-22 16:57:07 +0300

    (Feat) player finder

---
+++ players.js
@@ -0,0 +1,68 @@
+export default players = {
+  players: [
+    {
+      name: "DarkAngelCY",
+      archer: false,
+      tank: true,
+      healer: true,
+      mage: false,
+      bers: true
+    },
+    {
+      name: "Befahren",
+      archer: false,
+      tank: true,
+      healer: true,
+      mage: false,
+      bers: true
+    },
+    {
+      name: "NotDanzi",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: false,
+      bers: true
+    },
+    {
+      name: "PlaySkyBlock",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: true,
+      bers: true
+    },
+    {
+      name: "45ms_",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: false,
+      bers: true
+    },
+    {
+      name: "johnylau06",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: false,
+      bers: true
+    },
+    {
+      name: "Unnamed_2",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: true,
+      bers: true
+    },
+    {
+      name: "Hoofay",
+      archer: true,
+      tank: true,
+      healer: true,
+      mage: true,
+      bers: true
+    }
+  ] 
+}
